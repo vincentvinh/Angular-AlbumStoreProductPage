70cf2f542e439d38e74c4be7e86c7400579823b0
Vincent Christophe <vincent.christophe@gforces.auto>
2019-09-27 09:36:33 +0700

    test 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {ProductService} from './product.service';
@@ -9,6 +10,14 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+
+const appRoutes: Routes = [
+    { path: 'products', component: ProductListComponent },
+    { path: 'product/:id', component: ProductPageComponent },
+    { path: '', redirectTo: 'products', pathMatch: 'full'}
+];
+
+
 
 @NgModule({
   declarations: [
@@ -21,7 +30,8 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
